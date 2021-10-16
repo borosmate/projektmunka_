fbb853e36b129744d4db22eec6d9a2e81714aa95
Máté <boros.mate.foszk@gmail.com>
2021-10-16 22:48:52 +0200

    verfrissites

--- controller/bejelentkezes.php
+++ controller/bejelentkezes.php
@@ -1,24 +1,21 @@
 <?php
    require_once "/xampp/htdocs/settings/db.php";
-   
-   
+
+
    if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
+      $email = $_POST["email"];
+      $password = $_POST['password'];
+      $password = hash('sha512',$password . $salt);
+   
+
       
-    $email = $_POST["email"];
-    $password = $_POST['password'];
-    //$password = hash('sha512',$password);
-    $password_hash = hash('sha512',$password);
-      
-      $sql = "SELECT id FROM registration WHERE email = '$email' and password = '$password_hash'";
+      $sql = "SELECT id FROM registration WHERE email = '$email' and password = '$password'";
       $result = mysqli_query($DB,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
       
       $count = mysqli_num_rows($result);
       printf($count);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
 		
       if($count == 1) {
         
