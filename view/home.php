103a4b707eb43b5124ca6036fe00bfa42be187a9
Máté <boros.mate.foszk@gmail.com>
2021-10-21 12:14:13 +0200

    frissites panel

--- view/home.php
+++ view/home.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
+<div class="flex flex-wrap flex-col justify-center items-center max-w-sm mx-auto my-8">
   <div style="background-image: url(https://images.unsplash.com/photo-1539185441755-769473a23570?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80)"
        class="bg-indigo-300 h-64 w-full rounded-lg shadow-md bg-cover bg-center"></div>
   <div class="w-56 md:w-64 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
